8b62fd075ba1b1faad044446d42a7d27001a99c0
Abid Al Wassie <abidalwassie@outlook.com>
2025-09-27 11:15:03 +0600

    feat: integrate bundle analyzer for improved build analysis; update package.json and pnpm-lock.yaml to include necessary dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
+// Base Next.js config
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -12,4 +15,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
